5d0a1d6ecd09b22d0c2bdc5c0752f076d5b806f5
HazalCiplak <hazalciplak@gmail.com>
2022-11-18 12:13:57 +0000

    fixed return type

--- tests/dag_validation_test/conftest.py
+++ tests/dag_validation_test/conftest.py
@@ -8,5 +8,5 @@ DAG_FILES = [f for f in os.listdir(DAG_PATH) if f.endswith(".py")]
 
 
 @pytest.fixture(name="dagbag", scope="session")
-def _airflow_dagbag() -> dagbag:
+def _airflow_dagbag() -> dagbag.DagBag:
     return dagbag.DagBag(dag_folder=DAG_PATH, include_examples=False)
