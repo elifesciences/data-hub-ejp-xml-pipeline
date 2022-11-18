8a025a7c0f1a6e47fc3b9dd35ca261df7ee46d2e
HazalCiplak <hazalciplak@gmail.com>
2022-11-18 12:18:31 +0000

    fixed the issue 'None has no attributes ...'

--- ejp_xml_pipeline/data_store/bq_data_service.py
+++ ejp_xml_pipeline/data_store/bq_data_service.py
@@ -145,11 +145,11 @@ def get_new_merged_schema(
 ):
     new_schema = []
     existing_schema_dict = {
-        schema_object.get("name").lower(): schema_object
+        schema_object["name"].lower(): schema_object
         for schema_object in existing_schema
     }
     update_schema_dict = {
-        schema_object.get("name").lower(): schema_object
+        schema_object["name"].lower(): schema_object
         for schema_object in update_schema
     }
     merged_dict = {
@@ -165,8 +165,8 @@ def get_new_merged_schema(
     fields_to_recurse = [
         obj_key
         for obj_key in set_intersection
-        if existing_schema_dict.get(obj_key).get("fields") and
-        isinstance(existing_schema_dict.get(obj_key).get("fields"), list)
+        if existing_schema_dict[obj_key].get("fields") and
+        isinstance(existing_schema_dict[obj_key].get("fields"), list)
     ]
     new_schema.extend(
         [
@@ -176,10 +176,10 @@ def get_new_merged_schema(
         ]
     )
     for field_to_recurse in fields_to_recurse:
-        field = existing_schema_dict.get(field_to_recurse).copy()
+        field = existing_schema_dict[field_to_recurse].copy()
         field["fields"] = get_new_merged_schema(
-            existing_schema_dict.get(field_to_recurse).get("fields", []),
-            update_schema_dict.get(field_to_recurse).get("fields", []),
+            existing_schema_dict[field_to_recurse].get("fields", []),
+            update_schema_dict[field_to_recurse].get("fields", []),
         )
         new_schema.append(
             field
