39d545b7bc3b5800f340baad5ff07a59dcf53571
HazalCiplak <hazalciplak@gmail.com>
2024-11-14 17:56:44 +0000

    typo

--- run_test.sh
+++ run_test.sh
@@ -7,20 +7,20 @@ export PYTHONOPTIMIZE=
 
 echo "running pylint"
 PYLINTHOME=/tmp/datahub-dags-pylint \
- pyhton -m pylint tests/ ejp_xml_pipeline/
+ python -m pylint tests/ ejp_xml_pipeline/
 
 echo "running flake8"
-pyhton -m flake8 tests/ ejp_xml_pipeline/
+python -m flake8 tests/ ejp_xml_pipeline/
 
 echo "running mypy"
-pyhton -m mypy tests/ ejp_xml_pipeline/
+python -m mypy tests/ ejp_xml_pipeline/
 
 echo "running unit tests"s
-pyhton -m pytest tests/unit_test/ -p no:cacheprovider -s --disable-warnings
+python -m pytest tests/unit_test/ -p no:cacheprovider -s --disable-warnings
 
 if [[ $1  &&  $1 == "with-end-to-end" ]]; then
     echo "running end to end tests"
-    pyhton -m pytest tests/end2end_test/ -p no:cacheprovider -s
+    python -m pytest tests/end2end_test/ -p no:cacheprovider -s
 fi
 
 echo "done"
